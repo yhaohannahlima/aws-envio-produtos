136038298dfa44ef49ea14873a581bb83515db09
Pam18 <yha.lima@outlook.com>
2022-05-02 13:37:07 -0300

    feat: inicialização do projeto

---
+++ grupo6.spring.aws/src/test/java/com/produtos/grupo6/spring/aws/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produtos.grupo6.spring.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
