721571309f578db1494062116de5247ef9550a79
Franklin Percicotte <43394686+frankpercicotte@users.noreply.github.com>
2022-05-11 13:12:55 -0300

    includo ClientesDAO e PedidoDAO

--- grupo6.spring.aws/src/main/java/com/produtos/grupo6/spring/aws/DAO/PedidosDAO.java
+++ grupo6.spring.aws/src/main/java/com/produtos/grupo6/spring/aws/DAO/PedidosDAO.java
@@ -4,6 +4,6 @@ package com.produtos.grupo6.spring.aws.DAO;
 import org.springframework.data.repository.CrudRepository;
 import com.produtos.grupo6.spring.aws.model.Produtos;
 
-public interface ProdutosDAO extends CrudRepository<Produtos, Integer>{
+public interface PedidosDAO extends CrudRepository<Produtos, Integer>{
 	
 }
