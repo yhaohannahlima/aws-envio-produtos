22d21bd17d3abe879ccbd741cdece3e1c8ab397b
Guilherme Felipe Campos <gui_fc@hotmail.com>
2022-05-12 17:23:15 -0300

    Resolvendo problemas de front-parte 2

--- grupo6.spring.aws/src/main/java/com/produtos/grupo6/spring/aws/controller/MainController.java
+++ grupo6.spring.aws/src/main/java/com/produtos/grupo6/spring/aws/controller/MainController.java
@@ -92,9 +92,13 @@ public class MainController {
     }
 
     @GetMapping("/listar_clientes")
-    public String buscarDadosClientes(ModelMap model){    	
-    	List<Clientes> clientes = (List<Clientes>)clientesDAO.findAll();    	
-    	model.addAttribute("clientes",clientes);    	
+    public String buscarDadosClientes(ModelMap model){ 
+    	if(redis.isempty) {
+    		
+    		List<Clientes> clientes = (List<Clientes>)clientesDAO.findAll();   
+    		redisset()
+    		model.addAttribute("clientes",clientes);    	
+    	}
     	for( Clientes c : clientes) {    		
     		System.out.println(c.toString());
     	}    
